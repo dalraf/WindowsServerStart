54201eaed67fad70a39f843d7b0d9816b9c070e4
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-29 14:44:13 -0300

    Definido WindowsServerStart

---
+++ WindowsServerStart.py
@@ -0,0 +1,13 @@
+import subprocess
+import urllib
+from pathlib import Path
+
+path_local = Path('.')
+script_url = 'https://community.chocolatey.org/install.ps1'
+ps_script_name = 'install_chocolatey.ps1'
+ps_script_path = path_local / ps_script_name
+urllib.request.urlretrieve(script_url, ps_script_path)
+
+command = 'powershell.exe -noprofile -executionpolicy bypass -file ' + ps_script_path
+
+subprocess.call('', shell=True)
